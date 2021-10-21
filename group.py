eeefb6a2a90b0aa12432a54623cd21d303a006a2
Suncong Mao <672826138@qq.com>
2021-10-21 11:53:39 +0100

    Add dic

--- group.py
+++ group.py
@@ -1,5 +1,20 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-Suncong Mao is 23 and he is a student
 
-my_group =
+
+my_group = {
+    "Suncong Mao": {
+        "age": 23,
+        "job": "student",
+        "relations": {
+            "Tony":"friend"
+        }
+    },
+    "Tony": {
+        "age": 20,
+        "job": "student",
+        "relations": {
+            "Suncong Mao":"friend"
+        }
+    }
+}
